68d7a70b4489cfe72550fbaa287e23da2950e583
Scott <scott.tasker@nrk.no>
2019-03-05 20:23:31 +0100

    Add Sample Project

---
+++ NancyOwinServerDemo/NancyOwinServerDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NancyOwinServerDemo
+{
+    class Program
+    {
+        public static IWebHost _webApp;
+        private const string baseUrl = "http://localhost:9000";
+
+        static void Main(string[] args)
+        {
+            _webApp = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls(baseUrl)
+                .Build();
+
+            _webApp.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NancyOwinServerDemo/NancyOwinServerDemo/Server.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Nancy;
+using Nancy.Owin;
+
+namespace NancyOwinServerDemo
+{
+    public class ServerModule : NancyModule
+    {
+        public ServerModule()
+        {
+
+            Get("/", args => "Hello world!");
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
